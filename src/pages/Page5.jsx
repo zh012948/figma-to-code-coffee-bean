34b2528092f8e21c0b4fdc7ff80638ee55b4c535
Zeeshan <zh012948@gmail.com>
2025-05-27 15:26:28 +0500

    added

--- src/pages/Page5.jsx
+++ src/pages/Page5.jsx
@@ -5,24 +5,27 @@ import '../styles/Page5.css'
 function Page5() {
     return (
         <>
-            <div className="page5-container">
-                <div className="overlay"></div>
-                <div className="content-container">
-                    <div className="heading">
-                        Get a chance to have an Amazing morning
+
+            <div className="page5-wrapper">
+                <div className="page5-container">
+                    <div className="overlay"></div>
+                    <div className="content-container">
+                        <div className="heading">
+                            Get a chance to have an <br /> Amazing morning
+                        </div>
+
+                        <div className="desc3">
+                            We are giving you our one time opportunity to <br /> experience a better life with coffee
+                        </div>
+
+                        <div className="order-button">
+                            <button className='btnOrder5'>Order Now</button>
+                        </div>
                     </div>
 
-                    <div className="desc3">
-                        We are giving you our one time opportunity to experience a better life with coffee
+                    <div className="img-container">
+                        <img src={coffeeCup} alt="Coffee Cup" />
                     </div>
-
-                    <div className="order-button">
-                        <button className='btnOrder5'>Order Now</button>
-                    </div>
-                </div>
-
-                <div className="img-container">
-                    <img src={coffeeCup} alt="Coffee Cup" />
                 </div>
             </div>
         </>
